e02645a8174d7ecd05dd8b30112582b2c9e55247
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-06-09 22:42:26 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const TaskManager = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState({
+    description: '',
+    date: '',
+    time: '',
+    assignedUser: '',
+  });
+  const [editTask, setEditTask] = useState(null);
+
+  const handleInputChange = (e) => {
+    setNewTask({ ...newTask, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editTask) {
+      // Update existing task
+      const updatedTasks = tasks.map((task) => {
+        if (task.id === editTask) {
+          return { ...task, ...newTask };
+        }
+        return task;
+      });
+      setTasks(updatedTasks);
+      setEditTask(null);
+    } else {
+      // Add new task
+      const taskId = new Date().getTime().toString();
+      const newTaskWithId = { ...newTask, id: taskId };
+      setTasks([...tasks, newTaskWithId]);
+    }
+
+    setNewTask({
+      description: '',
+      date: '',
+      time: '',
+      assignedUser: '',
+    });
+  };
+
+  const handleEdit = (taskId) => {
+    const taskToEdit = tasks.find((task) => task.id === taskId);
+    setNewTask(taskToEdit);
+    setEditTask(taskId);
+  };
+
+  const handleDelete = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task Manager</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Task Description:
+          <input
+            type="text"
+            name="description"
+            value={newTask.description}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+
+        <label>
+          Date:
+          <input
+            type="date"
+            name="date"
+            value={newTask.date}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+
+        <label>
+          Time:
+          <input
+            type="time"
+            name="time"
+            value={newTask.time}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+
+        <label>
+          Assigned User:
+          <input
+            type="text"
+            name="assignedUser"
+            value={newTask.assignedUser}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+
+        <button type="submit">{editTask ? 'Update Task' : 'Add Task'}</button>
+      </form>
+
+      <h2>Tasks:</h2>
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <div>
+              <strong>Description:</strong> {task.description}
+            </div>
+            <div>
+              <strong>Date:</strong> {task.date}
+            </div>
+            <div>
+              <strong>Time:</strong> {task.time}
+            </div>
+            <div>
+              <strong>Assigned User:</strong> {task.assignedUser}
+            </div>
+            <div>
+              <button onClick={() => handleEdit(task.id)}>Edit</button>
+              <button onClick={() => handleDelete(task.id)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default TaskManager;
---
+++ src/login.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+
+const FormValidation = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const validateEmail = (email) => {
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return regex.test(email);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, email, password } = formData;
+    let formErrors = {};
+    if (name.trim() === '') {
+      formErrors.name = 'Name is required';
+    }
+
+    if (email.trim() === '') {
+      formErrors.email = 'Email is required';
+    } else if (!validateEmail(email)) {
+      formErrors.email = 'Invalid email format';
+    }
+
+    if (password.trim() === '') {
+      formErrors.password = 'Password is required';
+    }
+
+    setErrors(formErrors);
+
+    if (Object.keys(formErrors).length === 0) {      
+      console.log('Form submitted:', formData);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        {errors.name && <span className="error">{errors.name}</span>}
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        {errors.email && <span className="error">{errors.email}</span>}
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        {errors.password && <span className="error">{errors.password}</span>}
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default FormValidation;
